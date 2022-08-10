5c70afa332c4bf578a42624d8a11e6321459d4c0
Tanya Matseonzhek <matseonzhek@gmail.com>
2022-08-10 16:51:12 +0400

    Module6 task1 DONE

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,25 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String fur = hasFur ? "a" : "no";
+        String paws = numberOfPaws == 1 ? numberOfPaws + " paw" : numberOfPaws + " paws";
+        return "This animal is mostly "
+                + color + ". It has " + paws + " and " + fur
+                + " fur.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Animal("blue", 1213, false).getDescription());
+    }
 }
